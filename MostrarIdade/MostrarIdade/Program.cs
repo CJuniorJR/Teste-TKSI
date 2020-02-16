8219a275004196d22d07c695900bdedd7819c231
Claudio Junior <claudiodsesjunior@gmail.com>
2020-02-16 06:56:22 -0300

    Mostrar idade em dias

---
+++ MostrarIdade/MostrarIdade/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MostrarIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anos, meses, dias;
+            Console.WriteLine("Digite a quantidade de anos: ");
+            while (!int.TryParse(Console.ReadLine(), out anos))
+            {
+                Console.WriteLine("Numero invalido");
+            }
+
+            Console.WriteLine("Digite a quantidade de meses: ");
+            while (!int.TryParse(Console.ReadLine(), out meses))
+            {
+                Console.WriteLine("Numero invalido");
+            }
+
+            Console.WriteLine("Digite a quantidade de dias: ");
+            while (!int.TryParse(Console.ReadLine(), out dias))
+            {
+                Console.WriteLine("Numero invalido");
+            }
+
+            Console.WriteLine("Idade em dias: " + 365 * anos + 30 * meses + dias);
+        }
+    }
+}
