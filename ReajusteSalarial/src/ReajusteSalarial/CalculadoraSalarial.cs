7b7cf1b0a93e56138eb8253ab7be2f4106fbd634
Claudio Junior <claudiodsesjunior@gmail.com>
2020-02-16 06:32:42 -0300

    Classe CalculadoraSalarial

---
+++ ReajusteSalarial/src/ReajusteSalarial/CalculadoraSalarial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReajusteSalarial
+{
+    class CalculadoraSalarial
+    {
+        public double SalarioAtual { get; protected set; }
+        public double PercentualReajuste { get; protected set; }
+
+        private List<string> MensagensErro { get; set; }
+        private bool IsConsistente => MensagensErro.Count == 0;
+
+        public CalculadoraSalarial(double salarioAtual, double percentualReajuste)
+        {
+            MensagensErro = new List<string>();
+            SalarioAtual = salarioAtual;
+            PercentualReajuste = PercentualReajuste;
+
+            ValidarSalario();
+        }
+
+        private void ValidarSalario()
+        {
+            if (SalarioAtual < 0) MensagensErro.Add("Salario negativo");
+        }
+
+        public (List<string> mensagens, bool isValid) Calcular()
+        {
+            if (!IsConsistente) return (MensagensErro, false);
+
+            var novoSalario = new List<string>();
+            novoSalario.Add($"O salario de {SalarioAtual} com reajuste de {PercentualReajuste}% é igual a {(SalarioAtual + (SalarioAtual * PercentualReajuste / 100))}");
+
+            return (novoSalario, true);
+        }
+    }
+}
